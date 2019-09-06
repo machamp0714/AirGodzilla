cfa7f07b765eacaacb30bf099ac73f78be7629b8
25 macamp <rateliasudaisuki@gmail.com>
2019-09-06 16:33:28 +0900

    Add delete test to rooms request spec #1

--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Rooms API", type: :request do
   let(:user) { FactoryBot.create(:alice) }
-  let(:valid_params) { FactoryBot.attributes_for(:alice_room, user: user) }
-
+  
   describe "POST create" do
+    let(:valid_params) { FactoryBot.attributes_for(:alice_room, user: user) }
+
     context "when the request is valid" do
       it "creates a room" do
         expect {
@@ -16,6 +17,16 @@ RSpec.describe "Rooms API", type: :request do
 
         expect(response).to have_http_status 200
       end
+    end
+  end
+
+  describe "DELETE" do
+    let!(:room) { FactoryBot.create(:alice_room, user: user) }
+
+    it "delete a room" do
+      expect {
+        delete api_v1_room_path(room)
+      }.to change(Room, :count).by(-1)
     end
   end
 end
