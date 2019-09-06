d6d83344849a36bb939c892edc38b8e2fdb4df6b
25 macamp <rateliasudaisuki@gmail.com>
2019-09-06 15:04:20 +0900

    Add create action to Rooms Controller #1

---
+++ app/controllers/api/v1/rooms_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::RoomsController < ApplicationController
+
+  def create
+    @room = current_user.rooms.build(room_params)
+    if @room.save
+      render json: @room, status: :ok
+    else
+      render json: { error: @room.errors, is_success: false }, status: 422
+    end
+
+  end
+
+  private
+
+    def room_params
+      params.require(:room).permit(
+        :home_type,
+        :room_type,
+        :accommodate,
+        :bed_room,
+        :bath_room,
+        :listing_name,
+        :summary,
+        :address,
+        :is_tv,
+        :is_kitchen,
+        :is_air,
+        :is_heating,
+        :is_internet,
+        :price
+      )
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   namespace :api, format: 'json' do
     namespace :v1 do
+      resources :rooms, only: %i[index create]
+
       get 'me', to: 'users#me'
       post 'signup', to: 'users#create'
       post 'login', to: 'sessions#create'
