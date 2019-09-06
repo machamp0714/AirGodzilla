d1fabe206868e5274ad802f79cd3976e9a27d22d
25 macamp <rateliasudaisuki@gmail.com>
2019-09-06 14:07:14 +0900

    ユーザーの作成に失敗することをテスト

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,17 +5,30 @@ require 'rails_helper'
 RSpec.describe 'Users API', type: :request do
   let(:user) { FactoryBot.build(:alice) }
 
-  it "新しいユーザーを作成できること" do
-    expect {
+  describe 'POST /signup' do
+    it "新しいユーザーを作成できること" do
+      expect {
+        post api_v1_signup_path, params: {
+          user: {
+            name: user.name,
+            email: user.email,
+            password: user.password
+          }
+        }
+      }.to change(User, :count).by(1)
+
+      expect(response).to have_http_status 200
+    end
+
+    it "ユーザーの作成し失敗すること" do
       post api_v1_signup_path, params: {
         user: {
-          name: user.name,
-          email: user.email,
-          password: user.password
+          name: "",
+          email: "",
+          password: ""
         }
       }
-    }.to change(User, :count).by(1)
-
-    expect(response).to have_http_status 200
+      expect(response).to have_http_status 422
+    end
   end
 end
