920f1e301550aec17c6f07c1d48e1d15a0fa47de
25 macamp <rateliasudaisuki@gmail.com>
2019-09-06 23:04:15 +0900

    Create Photos request spec #3

--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -1,6 +1,8 @@
+include ActionDispatch::TestProcess
+
 FactoryBot.define do
   factory :photo do
-    image { "MyString" }
-    room { nil }
+    image { fixture_file_upload(Rails.root.join("spec", "fixtures", "R21.jpg")) }
+    association :room, factory: :alice_room
   end
 end
