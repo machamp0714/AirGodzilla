ae77f1911d329b70a651c157a8f8472d399815fe
25 macamp <rateliasudaisuki@gmail.com>
2019-09-06 10:00:45 +0900

    Modify User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  before_save :downcase_email
+
+  has_secure_password
+  has_secure_token :access_token
+
+  validates :name,
+            presence: true
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false }
+  validates :password,
+            presence: true,
+            length: { minumun: 8 }
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
