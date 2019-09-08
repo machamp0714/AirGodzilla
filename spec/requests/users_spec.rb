3e20fb5901f501d02c71d9345ab276ee11ad4dae
25 macamp <rateliasudaisuki@gmail.com>
2019-09-08 13:09:28 +0900

    Add add_card request spec #9

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -31,4 +31,33 @@ RSpec.describe 'Users API', type: :request do
       expect(response).to have_http_status 422
     end
   end
+
+  describe "add_card function" do
+    let(:user_have_not_stripe) { FactoryBot.create(:alice) }
+    let(:user_having_stripe) { FactoryBot.create(:bob) }
+
+    context "request is valid" do
+      it "adds a card when the stripe_id don't presence" do
+        post api_v1_add_card_path, params: {
+          access_token: user_have_not_stripe.access_token,
+          expire: "09/32",
+          number: "4242 4242 4242 4242",
+          cvv: "000"
+        }
+        expect(response).to have_http_status 200
+        expect(response.body).to include "Your card is saved"
+      end
+
+      it "updates a card when the stripe_id presences" do
+        post api_v1_add_card_path, params: {
+          access_token: user_having_stripe.access_token,
+          expire: "09/32",
+          number: "4242 4242 4242 4242",
+          cvv: "000"
+        }
+        expect(response).to have_http_status 200
+        expect(response.body).to include "Your card is saved"
+      end
+    end
+  end
 end
