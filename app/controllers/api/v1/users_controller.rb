7be0a3f7d395074ab18260e16f6031ed2fcc34ae
25 macamp <rateliasudaisuki@gmail.com>
2019-09-08 13:08:31 +0900

    Add add_card action to users_controller #9

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,6 +10,35 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def add_card
+    user = User.find(current_user.id)
+    if user.stripe_id.blank?
+      customer = Stripe::Customer.create(
+        email: user.email
+      )
+      user.stripe_id = customer.id
+      user.save
+    else
+      customer = Stripe::Customer.retrieve(user.stripe_id)
+    end
+
+    month, year = params[:expire].split('/')
+    new_token = Stripe::Token.create(
+      card: {
+        number: params[:number],
+        exp_month: month,
+        exp_year: year,
+        cvc: params[:cvc]
+      }
+    )
+    customer.sources.create(source: new_token.id)
+
+    render json: { message: 'Your card is saved', is_success: true }, status: :ok
+    # logger.debug user.errors.inspect
+  rescue Stripe::CardError => e
+    render json: { error: e.message, is_success: false }, status: 404
+  end
+
   private
 
   def user_params
