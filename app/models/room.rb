1e3d087a8366c514f745f422aabc3f5d71c82135
25 macamp <rateliasudaisuki@gmail.com>
2019-09-08 15:27:54 +0900

    Install geocode #1

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,6 +3,9 @@
 class Room < ApplicationRecord
   enum instant: { Request: 0, Instant: 1 }
 
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   belongs_to :user
   has_many :photos, dependent: :destroy
   has_many :reservations, dependent: :destroy
