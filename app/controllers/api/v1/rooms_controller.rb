6887339388e2246f05ac850dd397669cd7a0ea73
25 macamp <rateliasudaisuki@gmail.com>
2019-09-09 16:00:39 +0900

    Add show action to rooms controller #1

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -16,6 +16,33 @@ class Api::V1::RoomsController < ApplicationController
     end
   end
 
+  def show
+    room = Room.find(params[:id])
+
+    today = Date.today
+    reservations = room.reservations.where(
+      'room_id = ? AND (start_date > ? AND end_date > ?) AND status = ?',
+      params[:id], today, today, 1
+    )
+
+    unavailable_dates = reservations.map do |r|
+      (r.start_date.to_datetime..r.end_date.to_datetime).map do |date|
+        date.strftime('%Y-%m-%d')
+      end
+    end.flatten.to_set
+
+    if !room.nil?
+      room_serializer = RoomSerializer.new(
+        room,
+        image: room.cover_photo,
+        unavailable_dates: unavailable_dates
+      )
+      render json: { room: room_serializer, is_success: true }, status: :ok
+    else
+      render json: { error: 'Invalid ID', is_success: false }, status: 422
+    end
+  end
+
   def destroy
     Room.find(params[:id]).destroy
     head :no_content
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   namespace :api, format: 'json' do
     namespace :v1 do
-      resources :rooms, only: %i[index create destroy] do
+      resources :rooms, only: %i[index create show destroy] do
         resources :photos, only: %i[create destroy]
         resources :reservations, only: %i[create] do
           member do
