02d0e3332fc160cd89ab09f04f86322b29afd49c
25 macamp <rateliasudaisuki@gmail.com>
2019-09-10 11:14:31 +0900

    get rails api room list #13

--- front/src/components/Layout/Main.js
+++ front/src/components/Layout/Main.js
@@ -1,7 +1,19 @@
 import React from "react";
+import axios from "axios";
 
-const Main = () => {
-  return <div>Main</div>;
-};
+export default class Main extends React.Component {
+  componentDidMount() {
+    axios
+      .get("http://localhost:3001/api/v1/rooms")
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-export default Main;
+  render = () => {
+    return <div>Main</div>;
+  };
+}
