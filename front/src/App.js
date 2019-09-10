46b3370b96fbe45b6eb23a4a989c0e0645325fe9
25 macamp <rateliasudaisuki@gmail.com>
2019-09-10 19:48:58 +0900

    Materialize CSSを導入 #13

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import Main from "./components/Layout/Main";
 import Navbar from "./components/Layout/Navbar";
 
--- front/src/components/Layout/Navbar.js
+++ front/src/components/Layout/Navbar.js
@@ -1,9 +1,4 @@
 import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import SignedInLinks from "./SignedInLinks";
-import SignedOutLinks from "./SignedOutLinks";
 
 class Navbar extends React.Component {
   constructor(props) {
@@ -14,13 +9,7 @@ class Navbar extends React.Component {
   }
 
   render = () => {
-    return (
-      <AppBar position="static" color="primary">
-        <Toolbar>
-          <Typography variant="h1">AirGodzilla</Typography>
-        </Toolbar>
-      </AppBar>
-    );
+    return <h1>navbar</h1>;
   };
 }
 
