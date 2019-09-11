cef12c6a1f648d96c7b95d869c893205ccd025b4
25 macamp <rateliasudaisuki@gmail.com>
2019-09-11 16:06:36 +0900

    add skip_before_action method

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  skip_before_action :verify_authenticity_token
+
   include Authenticate
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
