acf68169aec0a64fbb2626bc34b2f265acf449e3
25 macamp <rateliasudaisuki@gmail.com>
2019-09-14 00:12:24 +0900

    全てのbefore_actionでCSRF_TOKENをcookieに格納

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :set_csrf_cookie
+
   include Authenticate
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
@@ -9,5 +11,9 @@ class ApplicationController < ActionController::API
 
   def render_404
     render json: { error: 'Invalid ID', is_success: false }, status: 404
+  end
+
+  def set_csrf_cookie
+    cookies[:CSRF_TOKEN] = form_authenticity_token
   end
 end
