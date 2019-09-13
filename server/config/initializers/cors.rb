85caf348fec9458c551800bccaa1ac19c529eb6c
25 macamp <rateliasudaisuki@gmail.com>
2019-09-14 00:31:26 +0900

    cookieを利用してrequestを行うことを示すため、credentialsオプションを変更1

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -13,6 +13,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
              headers: :any,
-             methods: %i[get post put patch delete options head]
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
+
+Rails.application.config.action_controller.forgery_protection_origin_check = false
