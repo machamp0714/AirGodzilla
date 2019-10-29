0b60c046d9ae9ee5e295423a9f6fd06384ba2988
25 macamp <rateliasudaisuki@gmail.com>
2019-10-29 23:56:28 +0900

    google map apiのテスト

--- front/src/components/Layout/Top.js
+++ front/src/components/Layout/Top.js
@@ -1,58 +1,47 @@
-import React from "react";
+import React, {useEffect} from "react";
+import ReactDOM from "react-dom";
 
 const Top = () => {
-  return <div>Top</div>;
+  let mapDisplay = React.createRef();
+
+  const [address, setAddress] = React.useState("Sydney, NSW");
+  const [map, setMap] = React.useState(null);
+
+  useEffect(() => {
+    setMap(
+      new window.google.maps.Map(ReactDOM.findDOMNode(mapDisplay.current), {
+        zoom: 8,
+        center: new window.google.maps.LatLng(-34.397, 150.644)
+      })
+    );
+  }, map);
+
+  const handleClick = () => {
+    const geocoder = new window.google.maps.Geocoder();
+
+    geocoder.geocode({address: address}, (results, status) => {
+      if (status === "OK") {
+        map.setCenter(results[0].geometry.location);
+        const marker = new window.google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert("Geocode was not successful");
+      }
+    });
+  };
+
+  return (
+    <div>
+      <div id="map" ref={mapDisplay} style={{width: 320, height: 480}} />
+
+      <div>
+        <input type="text" value={address} />
+        <input type="button" value="Encode" onClick={handleClick} />
+      </div>
+    </div>
+  );
 };
 
 export default Top;
-
-// const [address, setAddress] = React.useState("");
-// const [cordinates, setCordinates] = React.useState({ lat: null, lng: null });
-
-// // async?
-// const handleSelect = async (value) => {
-//   const results = await geocodeByAddress(value);
-//   const latLng = await getLatLng(results[0]);
-//   console.log(latLng);
-
-//   setAddress(value);
-//   setCordinates(latLng);
-// };
-
-// return (
-//   <div>
-//     <PlacesAutocomplete
-//       value={address}
-//       onChange={setAddress}
-//       onSelect={handleSelect}
-//     >
-//       {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-//         <div>
-//           <p>Latitude: {cordinates.lat}</p>
-//           <p>Longitude: {cordinates.lng}</p>
-
-//           <input {...getInputProps({ placeholder: "Types address" })} />
-
-//           <div>
-//             {loading ? <div>...loading</div> : null}
-
-//             {suggestions.map((suggestion, index) => {
-//               const style = {
-//                 backgroundColor: suggestion.active ? "#41b6e6" : "#fff"
-//               };
-
-//               return (
-//                 <div
-//                   {...getSuggestionItemProps(suggestion, { style })}
-//                   key={index}
-//                 >
-//                   {suggestion.description}
-//                 </div>
-//               );
-//             })}
-//           </div>
-//         </div>
-//       )}
-//     </PlacesAutocomplete>
-//   </div>
-// );
